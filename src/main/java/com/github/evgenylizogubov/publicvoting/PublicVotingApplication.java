5f47c4ff3b6b065c621c666287fe1496e1e160c8
Lizogubov Eugeny <noobikrus@gmail.com>
2024-07-25 11:07:52 +0500

    Init commit

---
+++ src/main/java/com/github/evgenylizogubov/publicvoting/PublicVotingApplication.java
@@ -0,0 +1,13 @@
+package com.github.evgenylizogubov.publicvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicVotingApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(PublicVotingApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/github/evgenylizogubov/publicvoting/PublicVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.evgenylizogubov.publicvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicVotingApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
